2a4064da977815bd35261ac58b9ad3dc9516a427
Pawel82S <theoptimizersgroup@gmail.com>
2021-06-28 20:30:39 +0200

    Added comments to better describe intentions in some cases.

--- src/message.rs
+++ src/message.rs
@@ -2,14 +2,17 @@ use std::time::SystemTime;
 
 use crate::UserID;
 
+/// Structure representing message.
 pub struct Message {
     from: UserID,
     to: UserID,
+    // NOTE: In future we shuld have also Time Zone here.
     time: SystemTime,
     content: String,
 }
 
 impl Message {
+    /// Creates new message with current system time.
     pub fn new(content: String, from: UserID, to: UserID) -> Self {
         Self {
             from,
@@ -19,18 +22,22 @@ impl Message {
         }
     }
 
+    /// Returns sender ID.
     pub fn from(&self) -> &UserID {
         &self.from
     }
 
+    /// Returns reviever ID.
     pub fn to(&self) -> &UserID {
         &self.to
     }
 
+    /// Time when message was sent.
     pub fn time(&self) -> &SystemTime {
         &self.time
     }
 
+    /// Message contents.
     pub fn content(&self) -> &String {
         &self.content
     }
--- src/user.rs
+++ src/user.rs
@@ -1,16 +1,21 @@
 use std::collections::HashSet;
 
+// NOTE: I've created separate type in case we want to change it for something more advanced in the
+// future.
 pub type UserID = u64;
 
+/// Structure representing user.
 #[derive(Debug)]
 pub struct User {
     id: UserID,
     password: String,
     friends: HashSet<UserID>,
     invitations: HashSet<UserID>,
+    // NOTE: In future we should have Time Zone information included.
 }
 
 impl User {
+    /// Creates empty user.
     pub fn new(id: UserID, password: String) -> Self {
         Self {
             id,
@@ -20,14 +25,17 @@ impl User {
         }
     }
 
+    /// Returns UserID number.
     pub fn id(&self) -> UserID {
         self.id
     }
 
+    /// Returns user password.
     pub fn password(&self) -> &String {
         &self.password
     }
 
+    /// Changes user password from current to new if current is the same as 'self.password'.
     pub fn change_password(&mut self, new_password: String, current_password: &String) -> bool {
         if self.password == *current_password {
             self.password = new_password;
@@ -37,14 +45,18 @@ impl User {
         }
     }
 
+    /// Returns set of user friends IDs
     pub fn friends(&self) -> &HashSet<UserID> {
         &self.friends
     }
 
+    /// Returns true if user is friend with id.
     pub fn has_friend(&self, id: &UserID) -> bool {
         self.friends.contains(id)
     }
 
+    /// Adds user friend. Returns true if user have invitation from 'id' and isn't already friend
+    /// with it, otherwise returns false. In case of success 'id' is removed from invitation set.
     pub fn add_friend(&mut self, id: UserID) -> bool {
         if !self.has_friend(&id) && self.has_invitation(&id) {
             self.invitations.remove(&id);
@@ -54,18 +66,23 @@ impl User {
         }
     }
 
+    /// Removes 'id' from friends set. Returns true if it was removed, false otherwise.
     pub fn remove_friend(&mut self, id: UserID) -> bool {
         self.friends.remove(&id)
     }
 
+    /// Returns set of UserID that have send invitations.
     pub fn invitations(&self) -> &HashSet<UserID> {
         &self.invitations
     }
 
+    /// Returns true if user has invitation from 'id', false otherwise.
     pub fn has_invitation(&self, id: &UserID) -> bool {
         self.invitations.contains(id)
     }
 
+    /// Adds invitation. Returns true if user doesn't have invitation and isn't already friend with
+    /// it, otherwise returns false.
     pub fn add_invitation(&mut self, id: UserID) -> bool {
         if !self.has_invitation(&id) && !self.has_friend(&id) {
             self.invitations.insert(id)
@@ -74,6 +91,7 @@ impl User {
         }
     }
 
+    /// Removes 'id' from invitation set. Returns true if it was removed, false otherwise.
     pub fn remove_invitation(&mut self, id: UserID) -> bool {
         self.invitations.remove(&id)
     }
