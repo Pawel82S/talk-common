d5603d89166a536f0f7bcb2d643f445d202292f8
Pawel82S <theoptimizersgroup@gmail.com>
2021-06-28 18:39:14 +0200

    Added port for communication between client and server side.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ mod user;
 pub use message::Message;
 pub use user::{User, UserID};
 
+pub const COMM_PORT: u16 = 7878; // 7878 is Rust typed on phone keybord
+
 #[cfg(test)]
 mod tests {
     #[test]
